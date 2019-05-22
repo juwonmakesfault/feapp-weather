7d63b4c054ee900e5ffdb1be6b1b02710111be07
kylin-1q84 <shadekj@naver.com>
2019-05-22 23:04:24 +0900

    Change Weather tag

--- src/Header.js
+++ src/Header.js
@@ -7,14 +7,7 @@ const Header = props => {
   return (
     <div>
       <Navigation />
-      <header className = "App-header">
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>여기는 {city} 입니다.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-        </a>
-      </header>
     </div>
   );
 }
